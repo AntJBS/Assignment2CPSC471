2d39e3d0a20e180be02730752731a172c1a2023e
AntJBS <4ajbrooks@gmail.com>
2026-07-26 16:59:38 -0700

    Completed receiver portion

--- rdt_receiver.py
+++ rdt_receiver.py
@@ -17,15 +17,28 @@ with open(output_file, "wb") as f:
         packet, sender_addr = sock.recvfrom(BUFFER_SIZE)
         # Fill in start
         # Parse packet and check checksum.
-        
+        pkt_type, seq_num, payload, is_valid = parse_packet(packet)
         # If DATA packet has expected seq, write payload and ACK it.
+        if pkt_type == "DATA":
+            if seq_num == expected_seq:
+                f.write(payload)
+                ack_pkt = make_packet("ACK", seq_num)
+                sock.sendto(ack_pkt, sender_addr)
 
-        # If DATA packet is duplicate, resend ACK for last_good_seq.
-
+                last_good_seq = seq_num
+                expected_seq = 1 - expected_seq
+            else:
+                # If DATA packet is duplicate, resend ACK for last_good_seq.
+                ack_pkt = make_packet("ACK", seq_num)
+                sock.sendto(ack_pkt, sender_addr)
         # If packet is corrupt, ignore it or resend last ACK.
-
+        if not is_valid:
+            continue
         # If FIN arrives, send FINACK and break.
-
+        if pkt_type == "FIN":
+            finack_pkt = make_packet("FINACK", seq_num)
+            sock.sendto(finack_pkt, sender_addr)
+            break
         # Fill in end
 sock.close()
 print("Receiver complete.")
